f19e888f9e278143ba48083cdefa43283134e0ac
mjburton <mjburton@mit.edu>
2016-11-19 10:53:53 -0500

    add solar cell model

--- solar/solar.py
+++ solar/solar.py
@@ -1,51 +1,64 @@
 " Simple Solar-Electric Powered Aircraft Model "
-from solar.solar_irradiance import get_Eirr
+from solar_irradiance import get_Eirr
 from gpkit import Model, Variable
 from gpkitmodels.aircraft.GP_submodels.wing import WingAero
 from gpkitmodels.environment.wind_speeds import get_windspeed
 from gpkitmodels.environment.air_properties import get_airvars
+from gpkitmodels.helpers import summing_vars
 
 class Aircraft(Model):
     "vehicle"
     def __init__(self):
 
         self.flight_model = AircraftPerf
+        self.solarcells = SolarCells()
+
+        self.components = [self.solarcells]
 
         Wstructures = Variable("W_{structures}", "lbf", "structural weight")
         fstructures = Variable("f_{structures}", 0.35, "-",
                                "fractional structural weight")
         Wpay = Variable("W_{pay}", 10, "lbf", "payload")
-        Wsolar = Variable("W_{solar}", "lbf", "solar cell weight")
         Wbatt = Variable("W_{batt}", "lbf", "battery weight")
-        W = Variable("W", "lbf", "aircraft weight")
+        Wtotal = Variable("W_{total}", "lbf", "aircraft weight")
         S = Variable("S", "ft**2", "planform area")
         b = Variable("b", "ft", "wing span")
         cmac = Variable("c_{MAC}", "ft", "mean aerodynamic chord")
         AR = Variable("AR", 27, "-", "aspect ratio")
-        W = Variable("W", "lbf", "aircraft weight")
-        Ssolar = Variable("S_{solar}", "ft**2", "solar cell area")
-        eta_solar = Variable("\\eta_{solar}", 0.2, "-",
-                             "Solar cell efficiency")
         eta_charge = Variable("\\eta_{charge}", 0.98, "-",
                               "Battery charging efficiency")
         eta_discharge = Variable("\\eta_{discharge}", 0.98, "-",
                                  "Battery discharging efficiency")
         hbatt = Variable("h_{batt}", 350, "W*hr/kg", "battery energy density")
         Ebatt = Variable("E_{batt}", "J", "total battery energy")
+        g = Variable("g", 9.81, "m/s**2", "gravitational constant")
+
+        constraints = [Wstructures >= Wtotal*fstructures,
+                       Wtotal >= (Wstructures + Wbatt + Wpay +
+                                  sum(summing_vars(self.components, "W"))),
+                       Wbatt >= Ebatt/hbatt*g,
+                       b**2 == S*AR,
+                       cmac == S/b,
+                       self.solarcells["S"] <= S,
+                       eta_charge == eta_charge,
+                       eta_discharge == eta_discharge]
+
+        Model.__init__(self, None, [constraints, self.components])
+
+class SolarCells(Model):
+    "solar cell model"
+    def __init__(self):
+
         rhosolar = Variable("\\rho_{solar}", 0.3, "kg/m^2",
                             "solar cell area density")
         g = Variable("g", 9.81, "m/s**2", "gravitational constant")
+        S = Variable("S", "ft**2", "solar cell area")
+        W = Variable("W", "lbf", "solar cell weight")
+        etasolar = Variable("\\eta_{solar}", 0.2, "-",
+                            "Solar cell efficiency")
 
-        constraints = [Wstructures >= W*fstructures,
-                       W >= Wstructures + Wsolar + Wbatt + Wpay,
-                       Wsolar >= rhosolar*Ssolar*g,
-                       Wbatt >= Ebatt/hbatt*g,
-                       Ssolar <= S,
-                       b**2 == S*AR,
-                       cmac == S/b,
-                       eta_solar == eta_solar,
-                       eta_charge == eta_charge,
-                       eta_discharge == eta_discharge]
+        constraints = [W >= rhosolar*S*g,
+                       etasolar == etasolar]
 
         Model.__init__(self, None, constraints)
 
@@ -128,11 +141,11 @@ class SteadyLevelFlight(Model):
         etaprop = Variable("\\eta_{prop}", etap, "-", "propulsive efficiency")
 
         constraints = [
-            aircraft["W"] <= (
+            aircraft["W_{total}"] <= (
                 0.5*state["\\rho"]*state["V"]**2*perf["C_L"]
-                * aircraft["S"]),
+                * aircraft["S_Mission, Aircraft"]),
             T >= (0.5*state["\\rho"]*state["V"]**2*perf["C_D"]
-                  *aircraft["S"]),
+                  *aircraft["S_Mission, Aircraft"]),
             perf["P_{shaft}"] >= T*state["V"]/etaprop]
 
         Model.__init__(self, None, constraints, **kwargs)
@@ -144,7 +157,7 @@ class Power(Model):
         Pacc = Variable("P_{acc}", 0.0, "W", "Accessory power draw")
 
         constraints = [
-            state["(E/S)_{irr}"]*static["\\eta_{solar}"]*static["S_{solar}"] >= (
+            state["(E/S)_{irr}"]*static["\\eta_{solar}"]*static["S_Mission, Aircraft, SolarCells"] >= (
                 Poper*state["t_{day}"] + static["E_{batt}"]
                 / static["\\eta_{discharge}"]),
             Poper == Poper,
